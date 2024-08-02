a3fc350924e26becb6ffb08b370f52a1ecf69283
Damiën <da.hofle@gmail.com>
2024-08-02 15:31:43 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [showClientIdPage, setShowClientIdPage] = useState(false);
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -23,29 +24,66 @@ function App() {
     client.models.Todo.delete({ id });
   }
 
+  function ClientIdPage() {
+    return (
+      <div style={{width: "434px", height: "886px", position: "relative", background: "white", fontFamily: "Arial, sans-serif"}}>
+        <div style={{width: "434px", height: "886px", left: "0px", top: "0px", position: "absolute", background: "#EEEEEE"}} />
+        <div style={{width: "361px", height: "65px", left: "36px", top: "225px", position: "absolute", background: "white", borderRadius: "32.50px"}} />
+        <div style={{width: "269px", height: "29px", left: "82px", top: "243px", position: "absolute", color: "black", fontSize: "26px", fontWeight: 700, lineHeight: "26px", wordWrap: "break-word"}}>
+          ↓Volg de instructies
+        </div>
+        <div style={{width: "321px", height: "76px", left: "57px", top: "746px", position: "absolute", background: "#003366", borderRadius: "17px"}} />
+        <div style={{width: "297px", height: "186px", left: "69px", top: "313px", position: "absolute", textAlign: "center", color: "black", fontSize: "20px", fontWeight: 700, lineHeight: "20px", wordWrap: "break-word"}}>
+          Om verbinding te kunnen maken met je store hebben wij een koppelingsnummer nodig.
+        </div>
+        <div id="koppelButton" style={{width: "254px", height: "29px", left: "91px", top: "769px", position: "absolute", color: "white", fontSize: "26px", fontWeight: 700, lineHeight: "26px", wordWrap: "break-word", cursor: "pointer"}}>
+          ↓Koppel mijn store!
+        </div>
+        <div style={{width: "153px", height: "76px", left: "224px", top: "600px", position: "absolute", background: "white", borderRadius: "17px"}} />
+        <div style={{width: "122px", height: "29px", left: "240px", top: "614px", position: "absolute", textAlign: "center", color: "#003366", fontSize: "26px", fontWeight: 700, lineHeight: "26px", wordWrap: "break-word"}}>
+          Client ID:
+        </div>
+        <div style={{width: "153px", height: "76px", left: "46px", top: "600px", position: "absolute", background: "white", borderRadius: "17px"}} />
+        <div style={{width: "137px", height: "29px", left: "54px", top: "609px", position: "absolute", textAlign: "center", color: "#003366", fontSize: "26px", fontWeight: 700, lineHeight: "26px", wordWrap: "break-word"}}>
+          Client secret:
+        </div>
+        <div style={{width: "361px", height: "0px", left: "37px", top: "394px", position: "absolute", border: "1px black solid"}} />
+        <img src="/assets/AdPal_logo_no_white.png" style={{width: "188px", height: "188px", left: "123px", top: "37px", position: "absolute"}} />
+        <img src="/assets/ezgif-5-675330dec9.gif" style={{width: "316px", height: "164px", left: "59px", top: "418px", position: "absolute"}} />
+        <div id="result"></div>
+      </div>
+    );
+  }
+
   return (    
     <Authenticator>
       {({ signOut }) => (
-    
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() =>deleteTodo(todo.id)}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-      <button onClick={signOut}>Sign out</button>
-    </main>
+        <main>
+          {showClientIdPage ? (
+            <ClientIdPage />
+          ) : (
+            <>
+              <h1>My todos</h1>
+              <button onClick={createTodo}>+ new</button>
+              <ul>
+                {todos.map((todo) => (
+                  <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
+                ))}
+              </ul>
+              <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                  Review next step of this tutorial.
+                </a>
+              </div>
+              <button onClick={() => setShowClientIdPage(true)}>Show Client ID Page</button>
+            </>
+          )}
+          <button onClick={signOut}>Sign out</button>
+        </main>
       )}
-      </Authenticator>
+    </Authenticator>
   );
 }
 
